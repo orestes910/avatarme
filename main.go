bbbaeaa04124ef204e5179c6050b20335e72d59e
unknown <timbrammer910@gmail.com>
2018-07-12 16:16:34 -0400

    Generate random color and create base image

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"crypto/rand"
 	"crypto/sha1"
 	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
 	"io"
+	"math/big"
 	"os"
 )
 
@@ -28,4 +34,38 @@ func main() {
 
 	fmt.Printf("%d \n", even)
 
+	// Generate random color
+	bigR, _ := rand.Int(rand.Reader, big.NewInt(255))
+	r := bigR.Uint64()
+	bigG, _ := rand.Int(rand.Reader, big.NewInt(255))
+	g := bigG.Uint64()
+	bigB, _ := rand.Int(rand.Reader, big.NewInt(255))
+	b := bigB.Uint64()
+	col := color.RGBA{
+		R: uint8(r),
+		G: uint8(g),
+		B: uint8(b),
+		A: 255,
+	}
+
+	// Generate avatar and color white
+	avatar := image.NewRGBA(image.Rect(0, 0, 50, 50))
+	draw.Draw(avatar, avatar.Bounds(), &image.Uniform{color.White}, image.ZP, draw.Src)
+
+	// Write avatar to file
+	f, _ := os.OpenFile("out.png", os.O_WRONLY|os.O_CREATE, 0600)
+	defer f.Close()
+	png.Encode(f, avatar)
+
+	// Range Over even
+	// if val = 1, set
+
+	/* Create avatar
+	avatar := image.NewRGBA(image.Rect(0, 0, 50, 50))
+	avatar.Set(2, 3, color.RGBA{255, 0, 0, 255})
+
+	f, _ := os.OpenFile("out.png", os.O_WRONLY|os.O_CREATE, 0600)
+	defer f.Close()
+	png.Encode(f, avatar)
+	*/
 }
